6a99d1e433adb7ac7969a36842a1653e8a51dedc
Vaibhav (Personal) <vaibhavpersonal@Vaibhav.Krishnakumar-mbp>
2021-12-20 19:50:41 +0000

    Day 5 Part 2

--- day5.scala
+++ day5.scala
@@ -7,13 +7,12 @@ object Day5 {
   case class Line(x1 : Int, y1 : Int, x2: Int, y2: Int)
 
   def main(args: Array[String]) : Unit = {
-    val grid : Seq[Seq[Int]] = Seq()
+
     val answer = Source.fromFile(filename).getLines()
-      .map(s => {
-        val s"$x1,$y1 -> $x2,$y2" = s
+      .map(in => {
+        val s"$x1,$y1 -> $x2,$y2" = in
         Line(x1.toInt, y1.toInt, x2.toInt, y2.toInt)
       })
-        .filter(isHorizontalOrVertical)
         .flatMap(toPointsOnLine)
         .toSeq
         .groupBy(identity)
@@ -22,22 +21,23 @@ object Day5 {
         .filter((k, v) => v > 1)
         .size
 
-        println(s"Answer to Part 1 is $answer")
-  }
-
-  private def isHorizontalOrVertical : (Line => Boolean) = {
-    line => line.x1 == line.x2 || line.y1 == line.y2
+        println(s"Answer to Part 2 is $answer")
   }
 
   private def toPointsOnLine : (Line => Seq[(Int, Int)]) = {
     line => if (line.x1 == line.x2) {
+      // vertical
       val (min, max) = minMax(line.y1, line.y2)
       for (y <- min to max) yield (line.x1, y)
     } else if (line.y1 == line.y2) {
+      // horizontal
       val (min, max) = minMax(line.x1, line.x2)
       for (x <- min to max) yield (x, line.y1)
     } else {
-      Seq()
+      // 45deg diagonal
+      val stepX = if (line.x1 < line.x2) then 1 else -1
+      val stepY = if (line.y1 < line.y2) then 1 else -1
+      (line.x1 to line.x2 by stepX).zip(line.y1 to line.y2 by stepY)
     }
   }
 
